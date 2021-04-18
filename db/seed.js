f5e241124f80f3663e4026db3b52a4dbb0f8155e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-04-17 23:38:57 -0500

    tags update

--- db/seed.js
+++ db/seed.js
@@ -2,13 +2,14 @@ const { client,
   getAllUsers, 
   createUser, 
   updateUser, 
-  createPost, 
-  updatePosts, 
+  createPost,  
   getAllPosts,
   getPostsByUser, 
   updatePost,
-  getUserById} = require("./index");
-
+  getUserById,
+  createTags,
+  createPostTag,
+  addTagsToPost } = require("./index");
 
 async function createInitialUsers() {
   try {
@@ -45,24 +46,26 @@ async function createInitialPosts() {
     await createPost({
       authorId: albert.id,
       title: "First Post",
-      content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+      content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#first", "#best", "#glory-days",]
     });
 
+    await createPost({
+      authorId: sandra.id,
+      title: "First Post",
+      content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#first", "#best", "#glory-days"]
+    });
+
+    await createPost({
+      authorId: glamgal.id,
+      title: "First Post",
+      content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#first", "#best", "#glory-days"]
+    });
+    return createPost
   } catch(error) {
     throw error
-  }
-}
-
-async function rebuildDB() {
-  try {
-    client.connect();
-
-    await dropTables();
-    await createTables();
-    await createInitialUsers();
-    await createInitialPosts()
-  } catch (error) {
-    throw error;
   }
 }
 
@@ -71,6 +74,8 @@ async function dropTables() {
     console.log("!!Starting to drop tables...!!");
 
     await client.query(`
+            DROP TABLE IF EXISTS post_tags;
+            DROP TABLE IF EXISTS tags;
             DROP TABLE IF EXISTS posts;
             DROP TABLE IF EXISTS users;
         `);
@@ -95,12 +100,23 @@ async function createTables() {
             active BOOLEAN DEFAULT true
         );
 
-          CREATE TABLE posts (
+        CREATE TABLE posts (
             id SERIAL PRIMARY KEY,
-            "authorId" INTEGER REFERENCES user(id) NOT NULL,
+            "authorId" INTEGER REFERENCES users(id) NOT NULL,
             title VARCHAR(255) NOT NULL,
             content TEXT NOT NULL,
             active BOOLEAN DEFAULT true
+        );
+
+        CREATE TABLE tags (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL
+        );
+
+        CREATE TABLE post_tags (
+          "postId" INTEGER REFERENCES posts(id),
+          "tagId" INTEGER REFERENCES tags(id),
+          UNIQUE ("postId", "tagId")
         );
      `)
     console.log("!!Finished building tables!!");
@@ -143,6 +159,44 @@ async function testDB() {
     console.log("!!Finished database tests!!");
   } catch (error) {
     console.error("!!Error testing database!!");
+    throw error;
+  }
+}
+
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      '#happy', 
+      '#worst-day-ever', 
+      '#youcandoanything',
+      '#catmandoeverything'
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+    console.log(postOne.id, postTwo.id, postThree.id)
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.log("Error creating tags!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+    await createInitialPosts();
+    await createInitialTags();
+  } catch (error) {
     throw error;
   }
 }
